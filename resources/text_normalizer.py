dbe27d5d418ff6b75d1b724eb84f95bce32c7475
Alex Nim <alex.nim@ibm.com>
2020-12-11 00:34:59 +0000

    making changes to installs and spacy model

--- resources/text_normalizer.py
+++ resources/text_normalizer.py
@@ -5,13 +5,15 @@ from resources.contractions import CONTRACTION_MAP
 import re
 from nltk.corpus import wordnet
 import collections
+import en_core_web_sm
 #from textblob import Word
 from nltk.tokenize.toktok import ToktokTokenizer
 from bs4 import BeautifulSoup
 
 tokenizer = ToktokTokenizer()
 stopword_list = nltk.corpus.stopwords.words('english')
-nlp = spacy.load('en', parse=True, tag=True, entity=True)
+nlp = en_core_web_sm.load()
+# nlp = spacy.load('en', parse=True, tag=True, entity=True)
 # nlp_vec = spacy.load('en_vectors_web_lg', parse=True, tag=True, entity=True)
 
 
